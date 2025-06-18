e884dba3bfd929070d16efe32fa6ea2c6e53b448
Quantum <russewelt@gmail.com>
2025-06-17 23:02:31 -0300

    added: image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    formats: ["image/webp", "image/avif"],
+    deviceSizes: [640, 768, 1024, 1280, 1600],
+    imageSizes: [16, 32, 48, 64, 96],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
